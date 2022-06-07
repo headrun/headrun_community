1fa46c156cdfdd87de47fc15773840310d1fc2b3
Sandhya-headrun <105707064+Sandhya-headrun@users.noreply.github.com>
2022-06-07 17:38:12 +0530

    Delete backend directory

--- backend/base/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-#@login_required(login_url='/admin/login/')
-def home(response):
-    return render(response, "home.html", {})
--- backend/base/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-from .settings import DjangoUtil; DjangoUtil.init()
-
-application = get_wsgi_application()
